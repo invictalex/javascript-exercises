267ddcaf2c5d62a84bba3d50f8f7b9e4322291d2
Alex Christodoulou <alexchristodoulou@ymail.com>
2022-05-10 13:24:04 +0100

    Completes second task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+function repeatString(string, num)
+{
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    
+    var newString = "";
 
-};
+    
+
+    for (let i=0; i<num; i++)
+    {
+        newString = newString.concat(string);
+    }
+
+    return newString;
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;

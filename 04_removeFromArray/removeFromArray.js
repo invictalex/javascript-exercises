b908f60903cd64379a80f7a37a966f33e5cbd269
Alex Christodoulou <alexchristodoulou@ymail.com>
2022-05-10 18:11:52 +0100

    Completes task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+function removeFromArray(arr, ...itemsToRemove)
+{
+    for (let i = 0; i < itemsToRemove.length; i++)
+    {
+        var item = itemsToRemove[i];
 
-};
+        itemIndex = arr.indexOf(item);
+
+        if (itemIndex >= 0)
+        {
+            var removed = arr.splice(itemIndex, 1);
+        }
+
+        
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

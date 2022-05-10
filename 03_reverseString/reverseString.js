22c4f1d8930a4e0a4fe2bbe59eabb9d9398647a9
Alex Christodoulou <alexchristodoulou@ymail.com>
2022-05-10 16:32:20 +0100

    Completes Task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(string)
+{
+    var strArr = string.split("");
 
-};
+    var strArrRev = strArr.reverse();
+
+    var reverseStr = strArrRev.join("");
+
+    return reverseStr;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
